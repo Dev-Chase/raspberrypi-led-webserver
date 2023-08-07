504b70a3cde2eed2e0338f14fc756de2d601070c
Dev-Chase <99377968+Dev-Chase@users.noreply.github.com>
2023-08-07 10:03:04 -0400

    Fixed JS Request URI's

--- static/index.js
+++ static/index.js
@@ -5,13 +5,13 @@ $(function(){
         "false": "#aa0000",
     };
     let led_on = false;
-    $.get("/led/status", status => {
+    $.get("/api/led/status", status => {
       led_on = status;
       $("#content").css("background-color", colours[status]);
     });
 
     $(".border-transition").on("click", e => {
-        $.post(`/led/${e.target.dataset.request}`, status => {
+        $.post(`/api/led/${e.target.dataset.request}`, status => {
             led_on = status;
             $("#content").css("background-color", colours[status]);
         });
