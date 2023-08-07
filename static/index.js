e56ad3296c8b2e4e427fd3a3434e162d5921f699
Dev-Chase <99377968+Dev-Chase@users.noreply.github.com>
2023-08-07 13:38:25 -0400

    Re-attempting SetInterval

--- static/index.js
+++ static/index.js
@@ -1,19 +1,22 @@
-function getAndSetStatus(led_on) {
-    $.get("/api/led/status", status => {
-      led_on = status;
-      $("#content").css("background-color", colours[status]);
-    });
-}
-
-
 $(function(){
     const colours = {
         "true": "green",
         "false": "#aa0000",
     };
     let led_on = false;
-    getAndSetStatus(led_on);
-    setInterval(getAndSetStatus(led_on), 5000);
+
+    $.get("/api/led/status", status => {
+      led_on = status;
+      $("#content").css("background-color", colours[status]);
+    });
+
+    setInterval(() => {
+        console.log("happneing");
+        $.get("/api/led/status", status => {
+          led_on = status;
+          $("#content").css("background-color", colours[status]);
+        });
+    }, 5000);
 
 
     $(".border-transition").on("click", e => {
