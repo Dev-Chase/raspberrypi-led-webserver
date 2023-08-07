c6bd0cdc821ed1ebbe20d035f9ec93a6d1eac0ac
Dev-Chase <99377968+Dev-Chase@users.noreply.github.com>
2023-08-07 09:21:12 -0400

    Fixed Routing for Port Forwarding

--- static/index.js
+++ static/index.js
@@ -5,13 +5,13 @@ $(function(){
         "false": "#aa0000",
     };
     let led_on = false;
-    $.get("http://localhost:8080/api/led/status", status => {
+    $.get("/api/led/status", status => {
       led_on = status;
       $("#content").css("background-color", colours[status]);
     });
 
     $(".border-transition").on("click", e => {
-        $.post(`http://localhost:8080/api/led/${e.target.dataset.request}`, status => {
+        $.post(`/api/led/${e.target.dataset.request}`, status => {
             led_on = status;
             $("#content").css("background-color", colours[status]);
         });
